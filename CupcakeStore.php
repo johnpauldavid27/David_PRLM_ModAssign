74db4f6177b021e19a661ca71d9cc06ce4456dcf
johnpauldavid27 <johnpauldavid.27@gmail.com>
2025-12-03 20:33:28 +0800

    Update CupcakeStore.php
    edited the prices to pesos (Php), added 3 new products/cupcakes, fixed the incrementation of stock with the new products

--- CupcakeStore.php
+++ CupcakeStore.php
@@ -4,38 +4,63 @@ $storeNameCC = "Sweet Bliss Cupcake Shop";
 $cupcakes = [
     [
         "name"  => "Chocolate Dream",
-        "price" => 10,
+        "price" => 40,
         "num_items" => 15,
         "desc"  => "A rich chocolate cupcake topped with creamy dark chocolate frosting.",
         "img"   => "choc_dream.jpg"
     ],
     [
         "name"  => "Vanilla Swirl",
-        "price" => 5,
+        "price" => 35,
         "num_items" => 0,
         "desc"  => "A fluffy vanilla cupcake with a smooth buttercream swirl.",
         "img"   => "vani_swirl.jpg"
     ],
     [
         "name"  => "Strawberry Burst",
-        "price" => 7,
+        "price" => 37,
         "num_items" => 20,
         "desc"  => "A fruity strawberry-infused cupcake with fresh strawberry icing.",
         "img"   => "strawb_burst.jpg"
     ],
     [
         "name"  => "Cookies & Cream",
-        "price" => 9,
+        "price" => 39,
         "num_items" => 0,
         "desc"  => "A crushed-cookie cupcake blended with vanilla cream frosting.",
         "img"   => "cookies&cream.jpg"
     ],
+    [ //3 new flavors
+        "name"  => "Red Velvet Bliss",
+        "price" => 38,
+        "num_items" => 12,
+        "desc"  => "A moist red velvet cupcake topped with smooth cream cheese frosting.",
+        "img"   => "rv_bliss.jpg"
+    ],
+    [
+        "name"  => "Lemon Delight",
+        "price" => 36,
+        "num_items" => 16,
+        "desc"  => "A lemon cupcake with refreshing citrus glaze and zest.",
+        "img"   => "lemon_del.jpg"
+    ],
+    [
+        "name"  => "Caramel Crunch",
+        "price" => 39,
+        "num_items" => 14,
+        "desc"  => "A cupcake drizzled with caramel and crunchy toffee bits.",
+        "img"   => "c_crunch.jpg"
+    ]
 ];
 $totalnum_items =
       $cupcakes[0]["num_items"]
     + $cupcakes[1]["num_items"]
     + $cupcakes[2]["num_items"]
-    + $cupcakes[3]["num_items"];
+    + $cupcakes[3]["num_items"]
+    // include the 3 new flavors
+    + $cupcakes[4]["num_items"]
+    + $cupcakes[5]["num_items"]
+    + $cupcakes[6]["num_items"];;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -47,6 +72,7 @@ $totalnum_items =
 </head>
 <body>
 <h1><?= $storeNameCC ?></h1>
+<?php include 'header.php' ?>
 <h3 class="menu-header">Cupcake Menu</h3>
 <table>
     <tr>
@@ -86,4 +112,5 @@ $totalnum_items =
 </div>
 </body>
 </html>
+
 
